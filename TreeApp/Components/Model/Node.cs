6a28a92daed18dc4d3599e5e820103b070c48704
parajulipeshal <peshal.parajuli@students.snow.edu>
2026-03-13 10:18:29 -0600

    node base setup

--- TreeApp/Components/Model/Node.cs
+++ TreeApp/Components/Model/Node.cs
@@ -1,4 +1,12 @@
 public class Node
 {
-    
+    public int Value {get; private set;}
+    public Node? Left {get; set;}
+    public Node? Right {get; set;}
+    public Node (int value, Node? left=null, Node? right=null)
+    {
+        Value= value;
+        Left= left;
+        Right= right;
+    }
 }
