d0217d1498fb5e9668360eba9dba64c15cea14c5
parajulipeshal <peshal.parajuli@students.snow.edu>
2026-03-13 23:51:40 -0600

    test removed

--- TreeApp.Test/UnitTest1.cs
+++ TreeApp.Test/UnitTest1.cs
@@ -2,15 +2,6 @@
 
 public class UnitTest1
 {
-    [Fact]
-    public void TestBSTInsertion()
-    {
-        //will be using this to check left and right insert
-    }
 
-    [Fact]
-    public void TestSingleNode()
-    {
-    
-    }
+
 }
